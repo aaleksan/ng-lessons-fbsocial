3bc53e67ecf2db2a11e628c34434bfec8be1ff47
Alex Aleksanyan <a.aleksanyan@neocodis.com>
2016-07-16 13:58:50 +0200

    small fix

--- app/facebook/facebook.js
+++ app/facebook/facebook.js
@@ -46,6 +46,7 @@ angular.module('ngSocial.facebook', ['ngRoute', 'ngFacebook'])
       $scope.welcomeMsg = "Welcome " + response.name;
       $scope.isLoggedIn = true;
       $scope.userInfo = response;
+      console.log(response);
     },
     function(err){
       $scope.welcomeMsg = "Please Log In";
@@ -53,6 +54,6 @@ angular.module('ngSocial.facebook', ['ngRoute', 'ngFacebook'])
   };
 
   refresh();
-  
+
 }]);
 
